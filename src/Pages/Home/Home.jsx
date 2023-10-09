4af3bffbf0c4e786d3cd51acb9797838ceb9d87f
Dambar Sing <programmer.gshi18@gmail.com>
2023-10-09 18:22:24 +0545

    Basic Home page with Route

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
-
+import './Home.css'
 const Home = () => {
   return (
-    <h1>This is Home Page</h1>
+    <body>
+    <header>
+        <h1>DambarGM</h1>
+        <nav>
+            <ul>
+                <li><a href="about">About</a></li>
+                <li><a href="portfolio">Portfolio</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="content">
+        <div id="about" class="section">
+            <h2>About Me</h2>
+            <p>This is a brief description of yourself.</p>
+        </div>
+
+        <div id="portfolio" class="section">
+            <h2>Portfolio</h2>
+            {/* <!-- Add your portfolio projects here --> */}
+        </div>
+
+        <div id="contact" class="section">
+            <h2>Contact Me</h2>
+            <p>Email: your@email.com</p>
+            <p>Phone: (123) 456-7890</p>
+        </div>
+    </div>
+
+    <footer>
+        <p>&copy; 2023 DambarGM</p>
+    </footer>
+</body>
   )
 }
 
